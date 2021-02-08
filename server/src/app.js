4f28720d17c7be68c966fbd84fc4c529429e92d8
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-02-08 14:57:30 -0500

    server can run

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter(db));
-app.use('/users', usersRouter(db));
+app.use('/', indexRouter);
+// app.use('/users', usersRouter());
 
 module.exports = app;
