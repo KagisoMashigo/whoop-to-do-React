171730fa7748077d069fc430ab130133c957faa1
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-02-09 15:07:41 -0500

    begin process of rendering lists

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,17 @@ const App = () => {
         state,
         dispatch
     } = useApplicationData();
-      const userList = state.users.map((user) => (<li key={user.id} > User Name: {user.username} / User Email: {user.email} </li>
+    const userList = state.users.map((user) => (<li key={user.id} > User Name: {user.username} / User Email: {user.email} </li>   
   ));
+  //   const lists = state.lists.map((list) => (<li key={list.id} > User Name: {list.username} / User Email: {list.email} </li>   
+  // ));
+
   return (<div className="App" >
     <h1> Users </h1>
 
     <ul> {userList} </ul>
 
-    <h1> Categories </h1>
+    <h1> Lists </h1>
 
     <ul> List </ul>
   </div >
