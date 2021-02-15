94b85723a1e0b1cebb85c7cbb253c138c9bcc753
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-02-15 16:52:23 -0500

    add todos to state and switch logic

--- client/src/hooks/reducer.js
+++ client/src/hooks/reducer.js
@@ -1,21 +1,21 @@
 export const SET_USERS = 'SET_USERS';
-export const SET_LISTS = 'SET_LISTS';
+export const SET_TODOS = 'SET_TODOS';
 
 const dataReducer = (state, action) => {
     console.log("INiTIAL ACTION", action)
     switch (action.type) {
         case SET_USERS:
-            console.log("DAT STATE", state)
+            console.log("DAT USER STATE", state)
             return {
                 ...state,
                 users: action.users,
                     loading: false,
             };
-        case SET_LISTS:
-            console.log("DAT STATE", state)
+        case SET_TODOS:
+            console.log("DAT TODO STATE", state)
             return {
                 ...state,
-                users: action.lists,
+                todos: action.todos,
                     loading: false,
             };
         default:
